c1f2a9a56c0c39c163e99797ddee746909c75f9d
Costin <lcc31@pm.me>
2021-03-15 15:18:43 +0200

    Homework 1 finished

---
+++ Homework1/Homework1/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class Car
+    {
+        public const string WelcomMessage = "Welcome to the Multi-Brand Car Manufacturer and Dealership!";
+
+        public string Model { get; set; }
+        public string TypesOfPackages { get; set; }
+        public int EnginePower { get; set; }
+
+        public static int amountOfCars = 0;
+
+
+        public Car(string model = "Mercedes", string typesOfPackages = "Entry", int enginePower = 120)
+        {
+            Model = model;
+            TypesOfPackages = typesOfPackages;
+            EnginePower = enginePower;
+            amountOfCars++;
+        }
+
+        public static int ChooseActions(int action)
+        {
+            Console.WriteLine("You chose: " + action);
+            switch (action)
+            {
+                case 1:
+                    CreateNewCar();
+                    break;
+
+                case 2:
+                    Console.WriteLine(AmountOfManufacturedCars());
+                    break;
+            }
+            return action;
+        }
+
+        public static string PrintCar(Car car)
+        {
+            return "The " + car.Model
+                            + " is from package " + car.TypesOfPackages
+                            + " and the engine power is: " + car.EnginePower;
+        }
+
+        public static int PrintChooseAction()
+        {
+            int choice = 0;
+            Console.WriteLine(
+                        "Choose an action: " +
+                        "(0) to quit " +
+                        "(1) to add a new car " +
+                        "(2) print the amount of manufactured cars ");
+            choice = int.Parse(Console.ReadLine());
+            return choice;
+        }
+
+        private static void CreateNewCar()
+        {
+            Console.WriteLine("You chose to create a new car: ");
+            string newCarModel = "";
+            string newCarTypesOfPackages = "";
+            int newCarEnginePower = 10000;
+
+            Console.WriteLine("What type of car do you want? Mercedes, Smart or Limousine");
+            newCarModel = Console.ReadLine();
+
+            Console.WriteLine("What type of package do you choose? Premium, Entry or Plus");
+            newCarTypesOfPackages = Console.ReadLine();
+
+            Console.WriteLine("How powerful want to be your engine power?");
+            newCarEnginePower = int.Parse(Console.ReadLine());
+
+            Car newCar = new Car(newCarModel, newCarTypesOfPackages, newCarEnginePower);
+            Console.WriteLine(newCarModel + " " + newCarTypesOfPackages + " " + newCarEnginePower);
+        }
+
+        public static string AmountOfManufacturedCars()
+        {
+            return $"The amount of cars manufactured are: " + amountOfCars;
+        }
+
+    }
+}
---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Car.WelcomMessage);
+            Console.WriteLine("");
+
+            Car car1 = new Car();
+            Console.WriteLine(car1.Model, car1.TypesOfPackages, car1.EnginePower);
+            Car car2 = new Car("Smart");
+            Console.WriteLine(Car.PrintCar(car2));
+            Car car3 = new Car("Limousine", "Premium");
+            Console.WriteLine(Car.PrintCar(car3));
+            Car car4 = new Car("Limousine", "Premium", 150);
+            Console.WriteLine(Car.PrintCar(car4));
+            Console.WriteLine("");
+
+            Console.WriteLine(Car.AmountOfManufacturedCars());
+            Console.WriteLine("");
+
+            int action = Car.PrintChooseAction();
+            while (action != 0)
+            {
+                action = Car.ChooseActions(action);
+                action = Car.PrintChooseAction();
+            }
+
+        }
+    }
+}
