b761fac52efd5a26d7c9b9a1f32249d3e3267f43
Costin <lcc31@pm.me>
2021-04-22 21:10:48 +0300

    Second part completed

--- Homework08-09/src/components/App/App.js
+++ Homework08-09/src/components/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Main from '../Main';
+import Nav from '../Nav';
+import 'semantic-ui-css/semantic.min.css';
 
 const App = () => {
   return (
     <div>
-      Boilerplate React Photo Album App
+      <Nav />
+      <Main />
     </div>
   );
 }
